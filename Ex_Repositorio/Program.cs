23f6b5279af2053350efd7290eb1e54e30e27e14
Jose Arthuro Teodoro <zearthuro@hotmail.com>
2019-02-07 10:46:55 -0200

    Adicionei a mensagem bom dia

--- Ex_Repositorio/Program.cs
+++ Ex_Repositorio/Program.cs
@@ -7,6 +7,7 @@ namespace Ex_Repositorio
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Bom dia");
         }
     }
 }
